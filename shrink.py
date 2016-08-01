ca839d373d4ca85a67b8f7448fd8b96e909f23aa
Harry G <harrygg@gmail.com>
2016-08-01 18:21:19 +0300

    Final version

--- shrink.py
+++ shrink.py
@@ -2,27 +2,27 @@
 import xml.etree.ElementTree as ET
 import zipfile, shutil, gzip
 
-excluded_ids_file = 'IDs.txt'
+included_ids_file = 'IDs.txt'
 epg_file = 'epg.xml'
 small_epg_file = 'sepg.xml'
 small_epg_zip_file = 'sepg.xml.gz'
-excluded_ids = []
+included_ids = []
 
 #Load the big EPG file
 tree = ET.parse(epg_file)
 root = tree.getroot()
 
 #Load IDs to be excluded
-with open(excluded_ids_file) as f:
-  excluded_ids = f.read().splitlines()
-print "%s channels will be included in the new EGP" % len(excluded_ids)
-#print excluded_ids
+with open(included_ids_file) as f:
+  included_ids = f.read().splitlines()
+print "%s channels will be included in the new EGP" % len(included_ids)
+#print included_ids
 
 #Remove channel nodes
 channels = root.findall('channel')
 for c in channels:
   id = c.get('id')
-  if id not in excluded_ids:
+  if id not in included_ids:
     root.remove(c)
     operation = "removed"
   else:
@@ -32,17 +32,11 @@ for c in channels:
 #Remove programme nodes
 programmes = root.findall('programme')
 for p in programmes:
-  if p.get('channel') not in excluded_ids:
+  if p.get('channel') not in included_ids:
     root.remove(p)
 
 #Write the output file 
 tree.write(small_epg_file, 'utf-8', True)   
 
-#with gzip.open(small_epg_zip_file, 'wb') as f:
-#  f.write(content)
 with open(small_epg_file, 'rb') as f_in, gzip.open(small_epg_zip_file, 'wb') as f_out:
   shutil.copyfileobj(f_in, f_out)
-    
-#z = zipfile.ZipFile(small_epg_zip_file, 'w')
-#z.write(small_epg_file)
-#z.close()
